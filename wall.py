e513218d7b63e126a717896abf135f3a8dfd4d0a
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-18 08:52:09 -0800

    Update wall.py

--- wall.py
+++ wall.py
@@ -1,12 +1,12 @@
-brickPos = []
+wallPos = []
 
 class Brick(Board):
 
 
 	def brickInit(self):
 		for i in range(20):
-			x = randint(1,17)
-			y = randint(1,17)	
+			x = randint(1,6)
+			y = randint(1,6)	
 			brickPos.append([x,y])
 		return
 
@@ -17,12 +17,12 @@ class Brick(Board):
 			x = brickPos[i][0]
 			y = brickPos[i][1]
 			
-			if(x*y!=1 and (gameArray[2*x+1][4*y+1]!="X" )):
-				gameArray[2*x+1][4*y+1] = "/" 
-				gameArray[2*x+1][4*y+2] = "/"
-				gameArray[2*x+1][4*y+3] = "/"
-				gameArray[2*x+1][4*y+4] = "/"
-				gameArray[2*x+2][4*y+1] = "/" 
-				gameArray[2*x+2][4*y+2] = "/"
-				gameArray[2*x+2][4*y+3] = "/"
-				gameArray[2*x+2][4*y+4] = "/"
+			if(x*y!=1 and (ga[2*x+1][4*y+1]!="X" )):
+				ga[2*x+1][4*y+1] ="$" 
+				ga[2*x+1][4*y+2] = "$"
+				ga[2*x+1][4*y+3] = "$"
+				ga[2*x+1][4*y+4] = "$"
+				ga[2*x+2][4*y+1] = "$"
+				ga[2*x+2][4*y+2] = "$"
+				ga[2*x+2][4*y+3] = "$"
+				ga[2*x+2][4*y+4] = "$"
