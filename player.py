e53451576e6464323f3c945452fb966e79e6eb49
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-19 08:46:21 -0800

    Update player.py

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ pps=[1,1]
 class Player(Person):
 
 	def drawPlayer(self,x,y):
-		if(gameArray[2*x+1][4*y+1]!="X" and gameArray[2*x+1][4*y+1]!="/"):
+		if(gameArray[2*x+1][4*y+1]!="X" and gameArray[2*x+1][4*y+1]!="1"):
 				gameArray[2*x+1][4*y+1] = "B" 
 				gameArray[2*x+1][4*y+2] = "B"
 				gameArray[2*x+1][4*y+3] = "B
@@ -12,7 +12,7 @@ class Player(Person):
        return
 
         def cp(self,x,y):
-             if(gameArray[2*x+1][4*y+1]=="X" or gameArray[2*x+1][4*y+1]=="$"
+             if(gameArray[2*x+1][4*y+1]=="X" or gameArray[2*x+1][4*y+1]=="1"
 	         return -1
              return 1
 		
