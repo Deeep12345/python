3410b1be1fb9b25887f5008b908583ba362dc7c1
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-18 20:08:05 -0800

    Update player.py

--- player.py
+++ player.py
@@ -12,7 +12,45 @@ class Player(Person):
 				gameArray[2*x+2][4*y+3] = "B"
 				gameArray[2*x+2][4*y+4] = "B"
        return
-     def cp(self,x,y):
-		       if(gameArray[2*x+1][4*y+1]=="X" or gameArray[2*x+1][4*y+1]=="/"):
-			                return -1
-		       return 1   
+        def cp(self,x,y):
+             if(gameArray[2*x+1][4*y+1]=="X" or gameArray[2*x+1][4*y+1]=="$"
+	         return -1
+             return 1
+		
+         def moveDown(self):
+	 	x = playerPos[0]
+	 	y = playerPos[1]
+	 	if(self.checkPos(x+1,y)>0):
+	 	     playerPos[0] += 1
+	 	     self.drawPlayer(playerPos[0],playerPos[1])
+	 	else:
+	 	     print("lost live")
+		return
+	 def moveUp(self):
+		x = playerPos[0]
+		y = playerPos[1]	
+		if(self.checkPos(x-1,y)>0):
+		     playerPos[0] -= 1
+		     self.drawPlayer(playerPos[0],playerPos[1])	
+		else:
+		     print("lost live")
+		return
+	 def moveLeft(self):
+		x = playerPos[0]
+		y = playerPos[1]		
+		if(self.checkPos(x,y-1)>0):
+			playerPos[1] -= 1
+			self.drawPlayer(playerPos[0],playerPos[1])
+		else:
+		         print("lost live")
+		return
+	def moveRight(self):
+		x = playerPos[0]
+		y = playerPos[1]
+		if(self.checkPos(x,y+1)>0):
+			playerPos[1] += 1
+			self.drawPlayer(playerPos[0],playerPos[1])	
+		else:
+		         print("lost live")
+		return
+		
