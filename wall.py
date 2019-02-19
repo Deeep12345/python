ec820f39e310d19b445a8c98f4ae37815290b05f
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-19 08:15:03 -0800

    Update wall.py

--- wall.py
+++ wall.py
@@ -1,28 +1,28 @@
+from random import randint
+from board import *
+
 wallPos = []
 
-class Brick(Board):
+class wall(Board):
 
 
-	def brickInit(self):
+	def wallinit(self):
 		for i in range(20):
 			x = randint(1,6)
 			y = randint(1,6)	
-			brickPos.append([x,y])
+			wallPos.append([x,y])
 		return
 
-	def drawBricks(self):
-		size = len(brickPos)
+	def drawwalls(self):
+		size = len(wallPos)
 		for i in range(size):
-			#print(i,"\n")
-			x = brickPos[i][0]
-			y = brickPos[i][1]
+			x = wallPos[i][0]
+			y = wallPos[i][1]
 			
 			if(x*y!=1 and (ga[2*x+1][4*y+1]!="X" )):
 				ga[2*x+1][4*y+1] ="$" 
 				ga[2*x+1][4*y+2] = "$"
 				ga[2*x+1][4*y+3] = "$"
-				ga[2*x+1][4*y+4] = "$"
 				ga[2*x+2][4*y+1] = "$"
 				ga[2*x+2][4*y+2] = "$"
 				ga[2*x+2][4*y+3] = "$"
-				ga[2*x+2][4*y+4] = "$"
