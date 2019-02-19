14ccc44f8a28cd92a811d26fac93c4b9f2e48cab
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-18 22:03:14 -0800

    Update person.py

--- person.py
+++ person.py
@@ -1,1 +1,18 @@
+import sys
+from board import *
+from player import *
+from wall import *
+from bomb import *
 
+
+inp=input("press any key w,a,s,d,b")
+if(inp == 'q'):
+		sys.exit(0)		#Quits
+	elif(inp == 's'):
+		pl.moveDown()	#moves the player down on pressing 's'
+	elif(inp == 'w'):
+		pl.moveUp()		#moves the player up on pressing 'w'
+	elif(inp == 'a'):
+		pl.moveLeft()	#moves the player left on pressing 'a'
+	elif(inp == 'd'):
+		pl.moveRight()	#moves the player right on pressing 'd'
