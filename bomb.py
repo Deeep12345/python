c05b67657ca71e2daf5ade91edb7f139d0b94ec8
Deeep12345 <47698465+Deeep12345@users.noreply.github.com>
2019-02-19 08:44:12 -0800

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,3 +1,8 @@
+from board import *
+from player import *
+from movements import *
+from wall import *
+
 bps=[0,0,-1] #the position of the bomb for x , y.
 b=[]
 class Bomb():
@@ -13,13 +18,56 @@ class Bomb():
       ga[2*x+1][4*y+1]=bps[2]
       ga[2*x+1][4*y+2]=bps[2]
       ga[2*x+1][4*y+3]=bps[2]
-      ga[2*x+1][4*y+4]=bps[2]
       ga[2*x+2][4*y+1]=bps[2]
       ga[2*x+2][4*y+2]=bps[2]
       ga[2*x+2][4*y+3]=bps[2]
-      ga[2*x+2][4*y+4]=bps[2]
     bps[2]-=1
     if(bps[2]==-1):
       self.explosion()
     return
+  def de(self,x,y):      #for the scene during explosion ,to represent the explosion scene
+
+		ga[2*x+1][4*y+1] = "m"
+		ga[2*x+1][4*y+2] = "m"
+		ga[2*x+1][4*y+3] = "m"
+		ga[2*x+2][4*y+1] = "m"
+		ga[2*x+2][4*y+2] = "m"
+		ga[2*x+2][4*y+3] = "m"		
+		return
   
+  def ae(self,x,y):        #for the scene after the explosion has occured
+    if(x==pps[0] and y==pps[1]):
+      print("lost live")
+    elif([x,y] in wallPos):
+      wallPos.remove([x,y])
+     
+   return
+  
+  def cp(self,x,y):
+    if(ga[2*x+1][4*y+1]=="X"):
+      return -1
+    return 1
+  
+  def explosion(self):
+		x = bps[0]
+		y = bps[1]
+		self.de(x,y)
+		self.ae(x,y)
+
+		if(self.cp(x-1,y)>0):
+			self.de(x-1,y)
+			self.ae(x-1,y)
+
+		if(self.cp(x+1,y)>0):
+			self.de(x+1,y)
+			self.ae(x+1,y)
+
+		if(self.cp(x,y-1)>0):
+			self.de(x,y-1)
+			self.ae(x,y-1)
+
+		if(self.cp(x,y+1)>0):
+			self.de(x,y+1)
+			self.ae(x,y+1)
+		return
+
